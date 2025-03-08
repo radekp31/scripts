cb2eefbc485ac21eed3ad406442bfdc5f20c92d8
radekp31 <76949775+radekp31@users.noreply.github.com>
2025-03-08 20:46:34 +0100

    Update download.sh

--- yt-dlp/download.sh
+++ yt-dlp/download.sh
@@ -1,1 +1,5 @@
+#Download and keep names
+youtube-dl -i -f mp4 --yes-playlist -o '/your/desired/path/%(title)s.%(ext)s' '<URL>'
+
+#Adjust quality
 sudo yt-dlp -f bestvideo+bestaudio --merge-output-format mp4 --audio-format mp3 --audio-quality 0  -o "/foo/bar/path/%(title)s.%(ext)s" "<URL>"
