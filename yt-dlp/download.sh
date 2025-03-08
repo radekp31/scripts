45674217dbb21b9d734c4c61b053eeca18fb66b5
radekp31 <76949775+radekp31@users.noreply.github.com>
2025-03-08 21:08:14 +0100

    Update download.sh

--- yt-dlp/download.sh
+++ yt-dlp/download.sh
@@ -1,5 +1,8 @@
 #Download and keep names
 youtube-dl -i -f mp4 --yes-playlist -o '/your/desired/path/%(title)s.%(ext)s' '<URL>'
 
+#Select 720p
+yt-dlp -f "bv*[height=720]+ba" -o "/foo/bar/path/%(title)s.%(ext)s" "<URL>" --format mp4
+
 #Adjust quality
 sudo yt-dlp -f bestvideo+bestaudio --merge-output-format mp4 --audio-format mp3 --audio-quality 0  -o "/foo/bar/path/%(title)s.%(ext)s" "<URL>"
